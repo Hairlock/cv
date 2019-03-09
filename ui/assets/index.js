3acf1da9a39257493c412c19d6ad4a8b343cc741
yannick <yannseal1@gmail.com>
2019-03-09 19:50:06 +0000

    started header component

--- ui/assets/index.js
+++ ui/assets/index.js
@@ -3,15 +3,18 @@ function main() {
 }
 
 if (module.hot) {
-  module.hot.dispose(function() {
+  module.hot.dispose(function () {
     console.log("dispose");
   });
 
-  module.hot.accept(function() {
+  module.hot.accept(function () {
     console.log("accept");
     document.body.innerHTML = ''
     main();
   });
 }
+
+// import './fonts/*.ttf';
+import './styles/index.scss';
 
 main();
--- ui/src/Utils.purs
+++
@@ -1,8 +0,0 @@
-module Utils where
-
-import Prelude
-import Halogen.HTML as HH
-import Halogen.HTML.Properties as HP
-
-css :: forall r i. String -> HH.IProp ( class :: String | r ) i
-css = HP.class_ <<< HH.ClassName
