4a515641bf4e4bbfe5e93e0c681f80b25d56520f
yannick <yannseal1@gmail.com>
2019-03-12 22:38:27 +0000

    footer targets set to blank

--- ui/src/Components/Footer.purs
+++ ui/src/Components/Footer.purs
@@ -27,21 +27,21 @@ footer =
                 [ HH.li
                     [ cs "footer-links__item" ]
                     [ HH.a
-                        [ HP.href "https://github.com/Hairlock", cs "footer-links__item-link" ]
+                        [ HP.href "https://github.com/Hairlock", cs "footer-links__item-link", HP.target "_blank" ]
                         [ HH.text "Github"
                         ]
                     ]
                 , HH.li
                     [ cs "footer-links__item" ]
                     [ HH.a
-                        [ HP.href "https://www.linkedin.com/in/yannick-sealy-b7585970", cs "footer-links__item-link" ]
+                        [ HP.href "https://www.linkedin.com/in/yannick-sealy-b7585970", cs "footer-links__item-link", HP.target "_blank" ]
                         [ HH.text "LinkedIn"
                         ]
                     ]
                 , HH.li
                     [ cs "footer-links__item" ]
                     [ HH.a
-                        [ HP.href "mailto: yannseal1@gmail.com", cs "footer-links__item-link" ]
+                        [ HP.href "mailto: yannseal1@gmail.com", cs "footer-links__item-link", HP.target "_blank" ]
                         [ HH.text "Email"
                         ]
                     ]
